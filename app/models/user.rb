4a4748cb5308e4d86f5700528bde3ecfe82d37cf
proofliu <proof.liu@gmail.com>
2018-03-15 14:48:44 +0800

    add dependency between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
   validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
 
-  has_many :comments
+  # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
+  has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
 
   def admin?
