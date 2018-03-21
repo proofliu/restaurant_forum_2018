74439066d2ce671a598cd3c5be1d161237a54352
proofliu <proof.liu@gmail.com>
2018-03-21 15:04:03 +0800

    user can view top10 favorited restaurant on ranking page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -53,4 +53,9 @@ class RestaurantsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
 
+  # GET /restaurants/ranking
+  def ranking
+    @restaurants = Restaurant.order(favorites_count: :desc).limit(10)
+  end
+
 end
