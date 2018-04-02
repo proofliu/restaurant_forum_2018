176ae631b630bd86ea98a4fbd266f4e4dd4fa664
proofliu <proof.liu@gmail.com>
2018-04-02 11:51:14 +0800

    validate uniqueness of following_id in user_id scope

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,4 +1,7 @@
 class Followship < ApplicationRecord
+  # 確認 (user_id, following_id) 的獨特性
+  validates :following_id, uniqueness: { scope: :user_id }
+
   belongs_to :user
   belongs_to :following, class_name: "User"
 end
